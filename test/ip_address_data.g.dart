c3ac0e7c0d025131739501c2729c796287dea8a0
nharris <nharris@easypost.com>
2022-12-01 01:07:38 -0500

    - Delete unneeded files

---
+++ test/ip_address_data.g.dart
@@ -0,0 +1,17 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'ip_address_data.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+IPAddressData _$IPAddressDataFromJson(Map<String, dynamic> json) =>
+    IPAddressData(
+      json['ip'] as String?,
+    );
+
+Map<String, dynamic> _$IPAddressDataToJson(IPAddressData instance) =>
+    <String, dynamic>{
+      'ip': instance.ipAddress,
+    };
